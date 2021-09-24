b0ecce179c43ff68ef5f3265747e67dbdff3cc52
Owens Ehimen <43546100+thelostprofessor@users.noreply.github.com>
2021-09-24 16:52:35 -0400

    Added methods to remove head or tail to LinkedList

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,18 @@ public class Main {
         LinkedList cities = new LinkedList();
         cities.addToHead(new Node("New York"));
         cities.addToHead(new Node("Shanghai"));
+
         cities.addToHead(new Node("Toronto"));
         cities.addToHead(new Node("Vancouver"));
-
-        cities.printList();
 
         cities.addToTail(new Node("Mississauga"));
         cities.addToTail(new Node("Oakville"));
         cities.addToTail(new Node("Ottawa"));
 
+        cities.printList();
+
+        cities.removeTail();
+        cities.removeTail();
         cities.printList();
     }
 }
--- src/datastructures/LinkedList.java
+++ src/datastructures/LinkedList.java
@@ -7,6 +7,7 @@ public class LinkedList {
         this.head = null;
     }
 
+    // Adding a node to the head of the list
     public void addToHead(Node newHead) {
         Node currentHead = this.head;
         this.head = newHead;
@@ -15,6 +16,7 @@ public class LinkedList {
         }
     }
 
+    // Adding a node to the tail of the list
     public void addToTail(Node newTail) {
         Node current = this.head;
         while (current.hasNextNode()) {
@@ -23,6 +25,37 @@ public class LinkedList {
         current.setNextNode(newTail);
     }
 
+    // Remove head of the list
+    public String removeHead() {
+        if (this.head == null) {
+            return null;
+        }
+        Node currentHead = this.head;
+        this.head = currentHead.getNextNode();
+        return currentHead.data;
+    }
+
+    // Remove the tail of the list
+    public String removeTail() {
+        if (this.head == null) {
+            return null;
+        }
+        if (!this.head.hasNextNode()) {
+            return this.removeHead();
+        }
+
+        Node current = this.head;
+        Node previous = null;
+        while (current.hasNextNode()) {
+            previous = current;
+            current = current.getNextNode();
+        }
+        previous.setNextNode(null);
+
+        return current.data;
+    }
+
+    // Prints a string representation of the list
     public void printList() {
         Node current = this.head;
         String stringRep = current.data + "-->";
