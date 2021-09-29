96f09c5968a3b5d19a03005f544cdd4d2f58e507
Owens Ehimen <43546100+thelostprofessor@users.noreply.github.com>
2021-09-29 17:13:01 -0400

    Created Vertex and edge classes for graphs

--- src/datastructures/Vertex.java
+++ src/datastructures/Vertex.java
@@ -11,14 +11,18 @@ public class Vertex {
         this.edges = new ArrayList<Edge>();
     }
 
+    // Adds a new edge to the edge list
     public void addEdge(Vertex newVertex) {
         this.edges.add(new Edge(newVertex));
     }
 
+    // Adds a new edge to the edge list
     public void addEdge(int cost, Vertex newVertex) {
         this.edges.add(new Edge(cost, newVertex));
     }
 
+    // Returns the index of the edge between this and endVertex. Returns -1 if no
+    // edge is found
     public int findEdge(Vertex endVertex) {
         int found = -1;
 
@@ -31,6 +35,8 @@ public class Vertex {
         return found;
     }
 
+    // Returns the cost of the edge between this and endVertex. Returns -1 if no
+    // edge is found
     public int getCost(Vertex endVertex) {
         int index = this.findEdge(endVertex);
         if (index == -1) {
