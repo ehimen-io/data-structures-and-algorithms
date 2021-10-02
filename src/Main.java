5f3f4bd385599fb99ea0e30763f684c859473ee0
Owens Ehimen <43546100+thelostprofessor@users.noreply.github.com>
2021-10-02 10:36:20 -0400

    Implemented the recursive mergesort method

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,33 @@
 
 import datastructures.*;
 
+import java.util.Arrays;
+
 import java.util.Random;
 
 import algorithms.*;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList cities = new LinkedList();
-        cities.addToHead(new Node("1"));
-        cities.addToHead(new Node("2"));
+        // LinkedList cities = new LinkedList();
+        // cities.addToHead(new Node("1"));
+        // cities.addToHead(new Node("2"));
 
-        cities.addToHead(new Node("4"));
-        cities.addToHead(new Node("3"));
+        // cities.printList();
 
-        cities.addToTail(new Node("7"));
-        cities.addToTail(new Node("6"));
-        cities.addToTail(new Node("5"));
+        // BubbleSort.bubbleSortList(cities);
 
-        cities.addToHead(new Node("10"));
-        cities.addToHead(new Node("15"));
-        cities.addToHead(new Node("23"));
-        cities.addToHead(new Node("0"));
-        cities.addToHead(new Node("68"));
+        // cities.printList();
 
-        cities.printList();
+        int[] numbers = new int[15];
+        Random rand = new Random();
 
-        BubbleSort.bubbleSortList(cities);
+        for (int i = 0; i < 15; i++) {
+            numbers[i] = rand.nextInt(40);
+        }
 
-        cities.printList();
+        System.out.println(Arrays.toString(numbers));
 
-        // int[] numbers = new int[15];
-        // Random rand = new Random();
-
-        // for (int i = 0; i < 15; i++) {
-        // numbers[i] = rand.nextInt(30);
-        // }
-
-        // BubbleSort.bubbleSortArray(numbers);
-        // for (int num : numbers) {
-        // System.out.print(num + ",");
-        // }
-        // System.out.print("\n");
-
+        System.out.println(Arrays.toString(MergeSort.sort(numbers)));
     }
 }
